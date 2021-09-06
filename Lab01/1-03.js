8b2602f3d9d65866df9200a261982070e133fa44
Harwex213 <harwex213@mail.ru>
2021-09-06 16:19:11 +0300

    fix(1-03.js): show body of request

--- Lab01/1-03.js
+++ Lab01/1-03.js
@@ -6,13 +6,32 @@ http.createServer((request, response) => {
     let headersString = "";
     for(const key in request.headers)
     {
-        headersString += "<p>" + key + ": " + request.headers[key];
+        headersString += "<p>" + key + ": " + request.headers[key] + "</p>";
     }
 
-    response.end("<p><b>Method: </b>" + request.method +
-        "<p><b>Http Version: </b>" + request.httpVersion +
-        "<p><b>Url: </b>" + request.url +
-        "<p><b>Headers: </b>" + headersString);
+    let body = ""
+    request.on("data", (data) => {
+        body += data;
+        console.log("body", body);
+    })
+    request.on("end", () =>  response.end(
+        `<!doctype html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport"
+                          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+                    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                    <title>Document</title>
+                </head>
+                <body>
+                    Method: ${request.method} <br>
+                    HTTP Version: ${request.httpVersion} <br>
+                    URI: ${request.url} <br>
+                    Headers: ${headersString} <br>
+                    Body: ${body} <br>
+                </body>
+                </html>`));
 }).listen(4000);
 
 console.log("We started");
