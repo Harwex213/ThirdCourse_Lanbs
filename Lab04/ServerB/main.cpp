b2ae2bf1c75a629417881a5b9d74c6a87d7a09ff
Harwex213 <harwex213@mail.ru>
2021-09-27 16:58:37 +0300

    feat(lab04): try to make broadcast message sending

--- Lab04/ServerB/main.cpp
+++ Lab04/ServerB/main.cpp
@@ -7,27 +7,62 @@
 using namespace std;
 using namespace connectionCustomizer;
 
+void FindAnotherServers(char* callSign)
+{
+    SOCKET broadcastSocket;
+    if ((broadcastSocket = socket(AF_INET, SOCK_DGRAM, NULL)) == INVALID_SOCKET)
+        throw Error::SetErrorMsgText("Open broadcast Socket:", WSAGetLastError());
+
+    int optionValue = 1;
+    if (setsockopt(broadcastSocket, SOL_SOCKET, SO_BROADCAST, (char*)&optionValue, sizeof(int) == SOCKET_ERROR))
+        throw Error::SetErrorMsgText("Set broadcast socket option:", WSAGetLastError());
+
+    SOCKADDR_IN broadcastSocketInfo;
+    broadcastSocketInfo.sin_family = AF_INET;
+    broadcastSocketInfo.sin_port = htons(2000);
+    broadcastSocketInfo.sin_addr.S_un.S_addr = INADDR_BROADCAST;
+    int broadcastSocketInfoLength = sizeof(broadcastSocketInfo);
+    char answer[50];
+
+    if (sendto(broadcastSocket,
+               callSign,
+               strlen(callSign) + 1,
+               NULL,
+               (sockaddr *) &broadcastSocketInfo,
+               sizeof(broadcastSocketInfo)) == SOCKET_ERROR)
+        throw Error::SetErrorMsgText("Send broadcast msg:",WSAGetLastError());
+
+    if (recvfrom(broadcastSocket,
+                 answer,
+                 sizeof(answer),
+                 NULL,
+                 (sockaddr *) &broadcastSocketInfo,
+                 &broadcastSocketInfoLength) == SOCKET_ERROR)
+        throw Error::SetErrorMsgText("Receive broadcast msg:",WSAGetLastError());
+
+    cout << answer << endl;
+
+    if (closesocket(broadcastSocket) == SOCKET_ERROR)
+        throw Error::SetErrorMsgText("Close broadcast Socket:", WSAGetLastError());
+}
+
 int main()
 {
     try {
-        SOCKET serverSocket;
         WSADATA wsaData;
+        if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
+            throw Error::SetErrorMsgText("Startup: ", WSAGetLastError());
+
+        SOCKET serverSocket;
+        if ((serverSocket = socket(AF_INET, SOCK_DGRAM, NULL)) == INVALID_SOCKET)
+            throw Error::SetErrorMsgText("OpenSocket:",WSAGetLastError());
+
         SOCKADDR_IN serverSocketInfo;
         serverSocketInfo.sin_family = AF_INET;
         serverSocketInfo.sin_port = htons(2000);
         serverSocketInfo.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
-
-        if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
-            throw Error::SetErrorMsgText("Startup: ", WSAGetLastError());
-        if ((serverSocket = socket(AF_INET, SOCK_DGRAM, NULL)) == INVALID_SOCKET)
-            throw Error::SetErrorMsgText("OpenSocket:",WSAGetLastError());
         if (bind(serverSocket, (LPSOCKADDR) &serverSocketInfo, sizeof(serverSocketInfo)) == SOCKET_ERROR)
             throw Error::SetErrorMsgText("Bind: ", WSAGetLastError());
-
-        cout << "server is open on: "
-             << inet_ntoa(serverSocketInfo.sin_addr)
-             << ":" << htons(serverSocketInfo.sin_port)
-             << endl;
 
         const char* callSign = "Hello";
         SOCKADDR_IN clientSocketInfo;
@@ -37,12 +72,19 @@ int main()
                                                                               &clientSocketInfo,
                                                                               &clientSocketInfoLength);
 
+//        FindAnotherServers((char *)callSign);
+
+        cout << "server is open on: "
+             << inet_ntoa(serverSocketInfo.sin_addr)
+             << ":" << htons(serverSocketInfo.sin_port)
+             << endl;
+
         while(true)
         {
             if(connectionCustomizer->IsExistRequestOnConnection())
             {
                 if (!connectionCustomizer->EstablishConnection())
-                    throw Error::SetErrorMsgText("OpenSocket:",WSAGetLastError());
+                    throw Error::SetErrorMsgText("Establishing connection:",WSAGetLastError());
 
                 cout << "Was established connection with "
                      << inet_ntoa(clientSocketInfo.sin_addr)
@@ -55,4 +97,5 @@ int main()
     catch (string& errorMsgText) {
         cout << endl << "WSAGetLastError: " << errorMsgText << endl;;
     }
+    system("pause");
 }
