59859149dfc8bf8e259942abc8dda32b1136e6b9
Harwex213 <harwex213@mail.ru>
2022-03-28 11:36:22 +0300

    feat(lab02): custom http error handling + layout header & footer

---
+++ Lab02/Controllers/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace Lab02.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult NotFound()
+        {
+            ViewBag.Method = Request.HttpMethod;
+            ViewBag.Uri = Request.Url?.AbsoluteUri.Substring(Request.Url.AbsoluteUri.IndexOf(';') + 1);
+            return View();
+        }
+
+    }
+}
